f1deb5a5ed81493296c1e1c4d7cbce99c9da2557
root <root@JOHN-LEGION5.localdomain>
2022-01-12 12:35:48 +0100

    c startup and correct linker script in minimal

--- minimal/blink.c
+++ minimal/blink.c
@@ -1,27 +1,62 @@
 // idea: https://www.youtube.com/watch?v=4vgnM9-EEeA&list=LL&index=1
+#include <stdint.h>
 
-void delay(int ms)
+// The current clock frequency
+uint32_t SystemCoreClock = 8000000;
+
+// Counts milliseconds
+volatile uint32_t systick_count;
+
+// Delay some milliseconds.
+// Note that effective time may be up to 1ms shorter than requested.
+void delay(uint32_t ms)
 {
-    volatile unsigned int j;
-    for (j=0; j < 1000UL * ms; j++)
-    {
+    uint32_t ends = systick_count + ms;
+    while (systick_count < ends);
+}
 
-    }
+// Interrupt handler
+void SysTick_Handler()
+{
+    systick_count++;
+}
+
+void SysTick_Config(uint32_t ticks)
+{
+    volatile uint32_t *systick_load = (uint32_t *)(0xE000E010 + 0x04);
+    volatile uint32_t *systick_val = (uint32_t *)(0xE000E010 + 0x08);
+    volatile uint32_t *systick_ctrl = (uint32_t *)(0xE000E010 + 0x00);
+
+    // Set reload register
+    *systick_load = ticks - 1;
+
+    // Set current value register
+    *systick_val = 0;
+
+    // Set the clock source of the system timer as AHBCLK=72M
+    // Enable system timer interrupt
+    *systick_ctrl  |= (1 << 1) | (1 << 2);
+
+    // Enable timer
+    *systick_ctrl  |= 1 << 0;
 }
 
 int main(void)
 {
-    volatile unsigned int *rcc_apb2enr = (unsigned int *)(0x40021000 + 0x18);
-    volatile unsigned int *gpioc_crh   = (unsigned int *)(0x40011000 + 0x04);
-    volatile unsigned int *gpioc_odr   = (unsigned int *)(0x40011000 + 0x0c);
+    volatile uint32_t *rcc_apb2enr = (uint32_t *)(0x40021000 + 0x18);
+    volatile uint32_t *gpioc_crh = (uint32_t *)(0x40011000 + 0x04);
+    volatile uint32_t *gpioc_odr = (uint32_t *)(0x40011000 + 0x0c);
+
+    // Initialize systick timer for 1 ms intervals
+    SysTick_Config(SystemCoreClock / 1000);
 
     // enable clock to GPIO port c
     *rcc_apb2enr |= 1 << 4;
 
     // configure PC13 pin for open drain output, 2 MHz max
-    *gpioc_crh = (*gpioc_crh & ~ (0xf << ((13 - 8) * 4 ))) | (6 << ((13 - 8) * 4));
+    *gpioc_crh = (*gpioc_crh & ~(0xf << ((13 - 8) * 4))) | (6 << ((13 - 8) * 4));
 
-    while(1)
+    while (1)
     {
         // simple wait
         delay(150);
--- minimal/startup.c
+++ minimal/startup.c
@@ -1,14 +1,73 @@
+#include <stdint.h>
+
 /* defined in linker script */
-extern unsigned int _estack;
+extern uint32_t _estack;
+
+extern uint32_t _etext;
+
+extern uint32_t _sdata;
+extern uint32_t _edata;
+
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+/* in blink.c */
 extern void main(void);
 
 void Reset_Handler(void) 
 {
+	/* Copy init values from text (=flash) to data (=ram) */
+    uint32_t *init_values_ptr = &_etext;
+    uint32_t *data_ptr = &_sdata;
+
+    if (init_values_ptr != data_ptr) {
+        for (; data_ptr < &_edata;) {
+            *data_ptr++ = *init_values_ptr++;
+        }
+    }
+
+    /* fill the the bss segment with all zero's */
+    for (uint32_t *bss_ptr = &_sbss; bss_ptr < &_ebss;) {
+        *bss_ptr++ = 0;
+    }
+
     main();
+
+    // main should never return!
+    while (1);
 }
+
+void Default_Handler(void)
+{
+	while(1);
+}
+
+void NMI_Handler 					(void) __attribute__ ((weak, alias("Default_Handler")));
+void HardFault_Handler 				(void) __attribute__ ((weak, alias("Default_Handler")));
+void MemManage_Handler 				(void) __attribute__ ((weak, alias("Default_Handler")));
+void BusFault_Handler 				(void) __attribute__ ((weak, alias("Default_Handler")));
+void UsageFault_Handler 			(void) __attribute__ ((weak, alias("Default_Handler")));
+void SVC_Handler 					(void) __attribute__ ((weak, alias("Default_Handler")));
+void DebugMon_Handler 				(void) __attribute__ ((weak, alias("Default_Handler")));
+void PendSV_Handler   				(void) __attribute__ ((weak, alias("Default_Handler")));
+void SysTick_Handler  				(void) __attribute__ ((weak, alias("Default_Handler")));
 
 const void *vectors[] __attribute__ ((section (".vectors"))) =
 {
     &_estack,
-    Reset_Handler
+    Reset_Handler,
+    NMI_Handler,
+    HardFault_Handler,
+	MemManage_Handler,
+	BusFault_Handler,
+	UsageFault_Handler,
+	0,
+	0,
+	0,
+	0,
+	SVC_Handler,
+	DebugMon_Handler,
+	0,
+	PendSV_Handler,
+	SysTick_Handler
 };
